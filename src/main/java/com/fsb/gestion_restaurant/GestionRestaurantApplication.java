6ef33cde3a62abdcbd9f5081734efea37e4e3b3e
Helmi <helmiben303@gmail.com>
2025-02-25 13:48:34 +0100

    Add all project files

---
+++ src/main/java/com/fsb/gestion_restaurant/GestionRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.fsb.gestion_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsb/gestion_restaurant/GestionRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsb.gestion_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
